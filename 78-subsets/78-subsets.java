1136959a8dda6d4512dccd22eb3928bb34feda34
Aditya Shrivastava <56548373+Shrivastava-Aditya@users.noreply.github.com>
2022-05-24 18:03:43 +0530

    Time: 1 ms (79.30%), Space: 43.3 MB (47.83%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,5 +1,11 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+        /*
+        Simple recursion approach with shifting the index single step at every recursive step
+        1. shift the index and again recursively solve. (discluding the current element)
+        2. store the element in array. (including the current element)
+        3. again recursively solve by incrementing the index
+        */
         List<List<Integer>> output = new ArrayList<>();
         
         findSubsets(output, nums, new ArrayList<>(), 0);
@@ -7,20 +13,20 @@ class Solution {
         return output;
         
     }
-    public static void findSubsets(List<List<Integer>> subset, int[] nums, ArrayList<Integer> output, int index)
+    public static void findSubsets(List<List<Integer>> output, int[] nums, ArrayList<Integer> list, int index)
     {
       // Base Condition
         if (index == nums.length) {
-            subset.add(output);
+            output.add(list);
             return;
         }
        
         // Not Including Value which is at Index
-        findSubsets(subset, nums, new ArrayList<>(output), index + 1);
+        findSubsets(output, nums, new ArrayList<>(list), index + 1);
  
         // Including Value which is at Index
-        output.add(nums[index]);
-        findSubsets(subset, nums, new ArrayList<>(output), index + 1);
+        list.add(nums[index]);
+        findSubsets(output, nums, new ArrayList<>(list), index + 1);
     }
     
 }
